11ad87a1a3371bbecf1773765683e37e84f427fa
LJT <251250208@smail.nju.edu.cn>
2026-07-02 17:04:07 +0800

    Use hash routing in desktop shell

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App.jsx";
+
+const isDesktopShell = window.location.protocol === "file:" || window.notewhaleDesktop;
+const app = isDesktopShell ? (
+  <HashRouter>
+    <App />
+  </HashRouter>
+) : (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
 createRoot(
   document.getElementById("root")
 ).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {app}
   </StrictMode>
 );
